28a884b46d758c7b0fe7abae90d2ffeffc8cd592
Sandy Huaringa <70159341@continental.edu.pe>
2020-06-21 18:40:56 -0500

    movimiento del personaje principal

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public PlayerMovement playerMov;
+    // Update is called once per frame
+    void Update()
+    {
+        float rotationValue = Input.GetAxis("Horizontal");
+
+        playerMov._rotationY = 45 * rotationValue;
+
+        float forwardValue = Input.GetAxis("Vertical");
+
+        playerMov._direction = new Vector3(x: 0, y: 0, z: 3 * forwardValue);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Vector3 _direction;
+    public float _rotationY;
+
+    // Update is called once per frame // 60 veces por segundo
+    void Update()
+    {
+        // distancia = velocidad por tiempo d = v * t
+        transform.Translate(_direction * Time.deltaTime, Space.Self);
+        transform.Rotate(new Vector3 (0f, _rotationY) * Time.deltaTime);
+    }
+}
