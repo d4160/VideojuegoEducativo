6c470c7488c826ac5c700aa960b23fefeb3f8c70
Susan1110 <72792382@continental.edu.pe>
2020-06-28 16:52:36 -0500

    Avance del personaje uno

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        float rotationValue = Input.GetAxis("Horizontal");
+        float forwardValue = Input.GetAxis("Vertical");
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Direccion hacia el frente o hacia atrás
+    public Vector3 _direction;
+    public float _rotationY;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // d = v * t
+        transform.Translate(_direction * Time.deltaTime, Space.Self);
+        transform.Rotate(new Vector3(0f,  _rotationY) * Time.deltaTime);
+    }
+}
