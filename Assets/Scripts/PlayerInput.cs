c4f81bc6f29313234ca44b3657773b36b6f27ad4
Susan1110 <72792382@continental.edu.pe>
2020-07-12 17:30:51 -0500

    Player backward walk animation

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -4,29 +4,18 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
-<<<<<<< HEAD
-=======
     public PlayerMovement playerMov;
 
->>>>>>> 811aa957afdfa22e1b64314adc2506fa8c4a77b6
     // Update is called once per frame
     void Update()
     {
         float rotationValue = Input.GetAxis("Horizontal");
-<<<<<<< HEAD
-        float forwardValue = Input.GetAxis("Vertical");
-=======
 
         playerMov._rotationY = rotationValue;
 
 
         float forwardValue = Input.GetAxis("Vertical");
 
-<<<<<<< HEAD
-        playerMov._direction = new Vector3(0, 0, 3 * forwardValue);
->>>>>>> 811aa957afdfa22e1b64314adc2506fa8c4a77b6
-=======
         playerMov._direction = new Vector3(0, 0, forwardValue);
->>>>>>> origin/diego
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,22 +4,17 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-<<<<<<< HEAD
-<<<<<<< HEAD
-    // Direccion hacia el frente o hacia atrás
-    public Vector3 _direction;
-    public float _rotationY;
-
-    // Update is called once per frame
-=======
-=======
     public float movementSpeed = 5;
     public float rotationSpeed = 135;
 
->>>>>>> origin/diego
     // Direccion hacia al frente o hacia atras
     public Vector3 _direction;
     public float _rotationY;
+
+    public float ZDirection()
+    {
+        return _direction.z;
+    }
 
     public bool IsWalking()
     {
@@ -27,23 +22,12 @@ public class PlayerMovement : MonoBehaviour
     }
 
     // Update is called once per frame // 60 veces por segundo
->>>>>>> 811aa957afdfa22e1b64314adc2506fa8c4a77b6
     void Update()
     {
-<<<<<<< HEAD
-        // d = v * t
-        transform.Translate(_direction * Time.deltaTime, Space.Self);
-<<<<<<< HEAD
-        transform.Rotate(new Vector3(0f,  _rotationY) * Time.deltaTime);
-=======
-        transform.Rotate(new Vector3(0f, _rotationY) * Time.deltaTime);
->>>>>>> 811aa957afdfa22e1b64314adc2506fa8c4a77b6
-=======
         // d = v * t * K
         transform.Translate(_direction * Time.deltaTime * movementSpeed, Space.Self);
 
         // r = euler * t * K
         transform.Rotate(new Vector3(0f, _rotationY) * Time.deltaTime * rotationSpeed);
->>>>>>> origin/diego
     }
 }
